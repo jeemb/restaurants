fe4ce64c955b08e6ca13c355dd887e8143ce7766
Jennifer Beem and Jayeson Kiyabu <jayeson@>
2017-02-22 13:33:17 -0800

    add app views web files for user interface

---
+++ app/app.php
@@ -0,0 +1,33 @@
+<?php
+    require_once __DIR__.'/../vendor/autoload.php';
+    require_once __DIR__.'/../src/Restaurant.php';
+    require_once __DIR__.'/../src/Cuisine.php';
+
+    $app = new Silex\Application();
+
+    $server = 'mysql:host=localhost:8889;dbname=eating';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    $app['debug'] = true;
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    $app->get("/", function() use ($app) {
+
+    return $app['twig']->render('index.html.twig', array('cuisines' => Cuisine::getAll()));
+    });
+
+    $app->post("/cuisine", function() use ($app) {
+        $type = $_POST['type'];
+        $cuisine = new Cuisine($type, $id = null);
+        $cuisine->save();
+        return $app['twig']->render('cuisine.html.twig', array('cuisine' => $cuisine));
+    });
+
+    return $app;
+
+?>
